eaefb3a24c6167103a6e60a7a284690064489605
bmraubo <bmraubo@gmail.com>
2021-09-27 15:51:19 +0100

    welcome_message tests and implementation

--- test_app.py
+++ test_app.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+from io import StringIO
+from tictactoe import *
 
 class TestDummy(unittest.TestCase):
 
@@ -7,6 +10,15 @@ class TestDummy(unittest.TestCase):
 
     def test_ci_workflow(self):
         self.assertEqual(True, True)
+
+class TestApplication(unittest.TestCase):
+
+    def test_welcome_message(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        Info.welcome_message()
+        output = captured_output.getvalue().strip()
+        self.assertEqual(output, 'Welcome to Tic Tac Toe')
 
 if __name__ == '__main__':
     unittest.main()
