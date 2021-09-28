d908f8f6cf1c2e272c23679d5c51fb40e4d04df8
bmraubo <bmraubo@gmail.com>
2021-09-28 17:10:48 +0100

    board init, create player

--- test_app.py
+++ test_app.py
@@ -13,6 +13,8 @@ class TestDummy(unittest.TestCase):
 
 
 class TestApplication(unittest.TestCase):
+
+    # Testing Info functions
     def test_welcome_message(self):
         captured_output = StringIO()
         sys.stdout = captured_output
@@ -27,6 +29,35 @@ class TestApplication(unittest.TestCase):
         validate_instructions = "Each square on the board have a value from 1-9. Select which square you would like to play by inputting the correct value when promoted."
         output = captured_output.getvalue().strip()
         self.assertEqual(output, validate_instructions)
+
+    # Test Players
+
+    # Test assign player to marker
+
+    # Testing Game Board
+
+    # Draw Board
+    def test_initialize_board(self):
+        expected_board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        test_board = Board()
+        self.assertEqual(test_board.board, expected_board)
+        self.assertEqual(test_board.markers["X"], None)
+        self.assertEqual(test_board.markers["O"], None)
+
+    # Create Players
+    def test_create_player(self):
+        test_board = Board()
+        test_board.create_player("Marx")
+        self.assertEqual(test_board.players[0].name, "Marx")
+
+    # Assign Players
+    def test_player_assignment(self):
+        test_board = Board()
+        test_player1 = Player("Marx")
+        test_player2 = Player("Engels")
+        test_board.assign_players(test_player1, test_player2)
+        self.assertEqual(test_board.markers["X"], test_player1)
+        self.assertEqual(test_board.markers["O"], test_player2)
 
 
 if __name__ == "__main__":
--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,25 @@ class Info:
         print(instructions)
 
 
+class Board:
+    def __init__(self):
+        self.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        self.markers = {"X": None, "O": None}
+        self.players = []
+
+    def create_player(self, name):
+        self.players.append(Player(name))
+
+    def assign_players(self):
+        # assigns players to X and O
+        pass
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+
 if __name__ == "__main__":
     Info.welcome_message()
     Info.game_instructions()
