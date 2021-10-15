dc7ec30a5a8619c09a7c2e6d10c7cc308d5c079f
bmraubo <bmraubo@gmail.com>
2021-10-15 13:10:35 +0100

    end to end testing

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -14,19 +14,24 @@ class TestTicTacToe(unittest.TestCase):
 
     # Initialise and Assign Players
     def test_create_player(self):
-        test_board = TicTacToe(3)
         test_players = [["Marx", "human", "X"], ["Engels", "human", "$"]]
+        test_board = TicTacToe(3, test_players)
         test_board.set_up_players(test_players)
         self.assertEqual(test_board.players[0].name, "Marx")
         self.assertEqual(test_board.players[0].type, "human")
         self.assertEqual(test_board.players[0].marker, "X")
 
     def test_player_assignment(self):
-        test_board = TicTacToe(3)
         test_players = [["Marx", "human", "X"], ["Engels", "human", "$"]]
-        test_board.set_up_players(test_players)
+        test_board = TicTacToe(3, test_players)
         self.assertEqual(test_board.markers[test_board.players[0]], "X")
         self.assertEqual(test_board.markers[test_board.players[1]], "$")
+
+    def test_end_to_end_computer_players(self):
+        test_players = [["Marx", "computer", "X"], ["Engels", "computer", "$"]]
+        test_board = TicTacToe(3, test_players)
+        test_board.play_game()
+        self.assertEqual(test_board.winner, test_board.players[0])
 
 
 if __name__ == "__main__":
