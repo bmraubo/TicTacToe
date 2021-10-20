746baaeb2b475abdf72b0232152c439d82f84bee
bmraubo <bmraubo@gmail.com>
2021-10-20 14:17:13 +0100

    new entry point added

---
+++ main.py
@@ -0,0 +1,11 @@
+from app.ui import UserInterface
+from app.tictactoe import TicTacToe
+
+if __name__ == "__main__":
+    UserInterface.display_welcome_message()
+    size = UserInterface.get_board_size()
+    players = UserInterface.get_player_info()
+    game = TicTacToe(size, players)
+    game.play_game()
+    print("Game is closing gracefully")
+    exit()
