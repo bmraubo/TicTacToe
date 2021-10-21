69e9e5ac84c36f93a89f27c9cf6912c9c474303b
bmraubo <bmraubo@gmail.com>
2021-10-21 17:22:28 +0100

    procfile and server host parameter

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import os
+import werkzeug
 
 from server.processmove import ProcessMove
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 
 def main():
     port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    app.run(host="0.0.0.0", port=port)
 
 
 @app.route("/", methods=["POST"])
