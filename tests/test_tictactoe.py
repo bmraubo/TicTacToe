e38262ef76057ee7b7383f3ba9acdc9b2564c70e
bmraubo <bmraubo@gmail.com>
2021-10-27 23:11:51 +0100

    simplified dummy test

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -6,9 +6,6 @@ class TestDummy(unittest.TestCase):
     def test_dummy(self):
         self.assertEqual(True, True)
 
-    def test_ci_workflow(self):
-        self.assertEqual(True, True)
-
 
 class TestTicTacToe(unittest.TestCase):
 
