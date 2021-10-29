f32db10c4cd464153a6bb5f95b8e8e70b237e255
bmraubo <bmraubo@gmail.com>
2021-10-29 14:46:41 +0100

    added successful move validation logic

--- app/server_process.py
+++ app/server_process.py
@@ -1,5 +1,7 @@
 from app.board import Board
 from app.player import Player
+from app.logic import Logic
+from app.util import Utilities
 
 
 class ServerProcess:
@@ -8,9 +10,17 @@ class ServerProcess:
         received_player = Player.create_server_player_object(request_data)
         received_move = request_data["move"]
         # Move Validation
-        # if move_validation[0]:
-        # Update Board Object
-        # Package {move_success}, {Board, Player}
+        move_validation = Logic.validate_move(
+            received_board, received_player, received_move
+        )
+        if move_validation[0]:
+            received_board.board_data = Utilities.change_board_value(
+                received_board.board_data, received_move, received_player.marker
+            )
+            payload = Utilities.generate_payload(
+                received_board, received_player, received_move
+            )
+            return {"move_success": True, "game_data": payload}
         # else:
         # Package response {{move_success}, {error reason}}
         pass
