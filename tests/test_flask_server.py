50f039d9f4e7814ddc402b1a5710d9a541086e16
bmraubo <bmraubo@gmail.com>
2021-11-01 13:08:35 +0000

    test_process returns status_code 200

--- tests/test_flask_server.py
+++ tests/test_flask_server.py
@@ -1,6 +1,9 @@
 import unittest
 from app.server_process import ServerProcess
 from flask import Flask, request
+from app.board import Board
+from app.player import HumanPlayer
+from app.util import Utilities
 
 
 class TestSimpleServer(unittest.TestCase):
@@ -18,4 +21,31 @@ class TestSimpleServer(unittest.TestCase):
         test_server = TestSimpleServer.create_test_server()
         with test_server.test_client() as client:
             response = client.post("/test")
+            self.assertEqual(response.status_code, 200)
+
+
+class TestGameServer(unittest.TestCase):
+    def create_test_game_server():
+        app = Flask(__name__)
+        app.testing = True
+
+        @app.route("/", methods=["POST"])
+        def test_process():
+            return "200", 200
+
+        return app
+
+    def server_process_test_set_up(test_move):
+        test_board = Board()
+        test_board.create_board(3)
+        test_player = HumanPlayer(["Marx", "human", "X"])
+        request_data = Utilities.generate_payload(test_board, test_player, test_move)
+        return request_data
+
+    def test_game_server_move(self):
+        test_server = TestGameServer.create_test_game_server()
+        with test_server.test_client() as client:
+            test_move = "1"
+            request_data = TestGameServer.server_process_test_set_up(test_move)
+            response = client.post("/", data=request_data)
             self.assertEqual(response.status_code, 200)
