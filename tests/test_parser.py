a6395ff015c82623b729924948d52b27e8165156
bmraubo <bmraubo@gmail.com>
2021-11-02 17:55:07 +0000

    fixed parser tests to avoid namespace

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,13 +6,13 @@ from app.tictactoe import TicTacToe
 class TestParser(unittest.TestCase):
     def test_parser(self):
         parser = Parser.create_parser()
-        test_server_mode = parser.parse_args(["-server"])
-        self.assertTrue(test_server_mode)
+        args = parser.parse_args(["-server"])
+        self.assertEqual(args.server, True)
 
     def test_start_game_in_server_mode(self):
         parser = Parser.create_parser()
-        test_server_mode = parser.parse_args(["-server"])
+        args = parser.parse_args(["-server"])
         test_players = [["Marx", "human", "X"], ["Engels", "human", "$"]]
         size = 3
-        test_game = TicTacToe(size, test_players, server=test_server_mode)
-        self.assertTrue(test_game.server)
+        test_game = TicTacToe(size, test_players, server=args.server)
+        self.assertEqual(test_game.server, True)
