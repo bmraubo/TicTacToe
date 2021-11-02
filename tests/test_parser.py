ae936b7e64ee24d9005f49a06df541688882fe24
bmraubo <bmraubo@gmail.com>
2021-11-02 15:58:35 +0000

    some changes to the test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,8 @@
 import unittest
-from app.tictactoe import TicTacToe
+from app.parser import Parser
 
 
 class TestParser(unittest.TestCase):
     def test_parser(self):
-        test_parser = Parser.create_parser()
-        test_server_mode = test_parser.parse_args("-server")
+        test_server_mode = Parser.run_parser()
         self.assertTrue(test_server_mode)
