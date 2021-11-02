0821811ca5d60bc619c9e12ea78ec07db4b0fe7d
bmraubo <bmraubo@gmail.com>
2021-11-02 17:56:17 +0000

    updated implementation in main

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from app.parser import Parser
 
 if __name__ == "__main__":
     parser = Parser.create_parser()
-    server_mode = parser.parse_args()
+    args = parser.parse_args()
     UserInterface.display_welcome_message()
     size = UserInterface.get_board_size()
     players = UserInterface.get_player_info()
     UserInterface.display_game_instructions(size)
-    game = TicTacToe(size, players, server=server_mode)
+    game = TicTacToe(size, players, server=args.server)
     game.play_game()
     print("Game is closing gracefully")
     exit()
