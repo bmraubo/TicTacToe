f08836a447c41d738c93db82ace37cc185fc52d2
bmraubo <bmraubo@gmail.com>
2021-11-02 16:20:51 +0000

    starting game in server mode

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from app.ui import UserInterface
 from app.tictactoe import TicTacToe
+from app.parser import Parser
 
 if __name__ == "__main__":
+    parser = Parser.create_parser()
+    server_mode = parser.parse_args()
     UserInterface.display_welcome_message()
     size = UserInterface.get_board_size()
     players = UserInterface.get_player_info()
     UserInterface.display_game_instructions(size)
-    game = TicTacToe(size, players)
+    game = TicTacToe(size, players, server=server_mode)
     game.play_game()
     print("Game is closing gracefully")
     exit()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from app.parser import Parser
+from app.tictactoe import TicTacToe
 
 
 class TestParser(unittest.TestCase):
@@ -7,3 +8,11 @@ class TestParser(unittest.TestCase):
         parser = Parser.create_parser()
         test_server_mode = parser.parse_args(["-server"])
         self.assertTrue(test_server_mode)
+
+    def test_start_game_in_server_mode(self):
+        parser = Parser.create_parser()
+        test_server_mode = parser.parse_args(["-server"])
+        test_players = [["Marx", "human", "X"], ["Engels", "human", "$"]]
+        size = 3
+        test_game = TicTacToe(size, test_players, server=test_server_mode)
+        self.assertTrue(test_game.server)
