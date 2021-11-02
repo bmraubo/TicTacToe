c9e324512c7cb1de56ab6097f0e37fcdcf106ea1
bmraubo <bmraubo@gmail.com>
2021-11-02 15:59:23 +0000

    args included in test...

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,5 +5,5 @@ from app.parser import Parser
 class TestParser(unittest.TestCase):
     def test_parser(self):
         args = "-server"
-        test_server_mode = Parser.run_parser()
+        test_server_mode = Parser.run_parser(args)
         self.assertTrue(test_server_mode)
